2c2c61579584a312ab792fb3050359629ad4e029
Anderson <anderson.bengolea@gmail.com>
2021-07-27 21:19:22 -0500

    adding sections and adapting to devices

---
+++ app.js
@@ -0,0 +1,19 @@
+gsap.from('#person', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:2})
+gsap.from('#line', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:1.5})
+gsap.from('.right', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10,delay: 2, opacity:0})
+gsap.from('.info-container', {duration:1, x: '30%', ease: "slow(0.7, 0.7, false)", x: -100, opacity:0, delay:1})
+gsap.from('.nav-container', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:2})
+
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    }else{
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
