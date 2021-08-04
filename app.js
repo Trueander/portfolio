a833f8e1d3ade59353996bb568b05f1bbff3b755
Anderson <anderson.bengolea@gmail.com>
2021-08-04 00:19:33 -0500

    new sections and new scroll

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ gsap.from('.right', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10
 gsap.from('.info-container', {duration:1, x: '30%', ease: "slow(0.7, 0.7, false)", x: -100, opacity:0, delay:1})
 gsap.from('.nav-container', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:2})
 
+gsap.from('#envelope', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:0.35})
+gsap.from('#paper', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -20, opacity:0, delay:0.7})
+
 
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
