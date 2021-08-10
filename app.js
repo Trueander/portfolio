de5c20b47f09cb588ee6ce4301add322a487b592
Anderson <anderson.bengolea@gmail.com>
2021-08-09 22:17:24 -0500

    new navbar, anim

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
-gsap.from('#person', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:2})
-gsap.from('#line', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:1.5})
-gsap.from('.right', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10,delay: 2, opacity:0})
-gsap.from('.info-container', {duration:1, x: '30%', ease: "slow(0.7, 0.7, false)", x: -100, opacity:0, delay:1})
-gsap.from('.nav-container', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:2})
 
-gsap.from('#envelope', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -10, opacity:0, delay:0.35})
-gsap.from('#paper', {duration:1, y: '10%', ease: "slow(0.7, 0.7, false)", y: -20, opacity:0, delay:0.7})
+gsap.timeline ({
+    defaults: {
+        duration:4,
+        repeat:-1,
+        stagger: 0.05,
+        ease: 'elastic.inOut(3)'
+    }
+})
+.to('.icon', {
+    opacity:1,
+    y: 50
+})
+.from('.icon', {
+    opacity:0,
+    y: 70
+})
 
+let lastScrollTop = 0;
+
+let navbar = document.getElementById("navbar");
+
+
+window.addEventListener("scroll", function(){
+
+
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+
+    if(scrollTop > lastScrollTop){
+        navbar.style.top = "-10vh";
+    }else {
+        navbar.style.top="0"
+        navbar.style.boxShadow = "0 10px 30px rgb(7 20 40)";
+        navbar.style.backdropFilter = "blur(10px)";
+        navbar.style.background = "#071a37bd";
+    }
+    lastScrollTop = scrollTop;
+
+    if(scrollTop == 0){
+        navbar.style.boxShadow = "none";
+        navbar.style.backdropFilter = "none";
+        navbar.style.background = "transparent";
+    }
+
+})
 
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
@@ -20,3 +57,5 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+
+new WOW().init();
